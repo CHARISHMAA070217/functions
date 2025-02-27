3e13efc347127e880b038c0b1853d05d83d646d0
CHARISHMAA070217 <charishma7118@gmail.com>
2025-02-27 13:49:43 +0530

    Create FUN1.py

---
+++ FUN1.py
@@ -0,0 +1,18 @@
+def fun1():
+    print("AAA")
+def fun2():
+    print("BBB")
+def fun3(a,b):
+    print("CCC")
+    print(a)
+    print(b)
+fun1()
+fun2()
+fun3(10,20)
+
+    **OUTPUT**
+AAA
+BBB
+CCC
+10
+20
